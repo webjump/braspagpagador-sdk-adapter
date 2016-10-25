f3f7c006b4b0e7bbb29a5503703f3e065d95dff1
Leandro Rosa <dev.leandrorosa@gmail.com>
2016-10-25 20:42:35 -0200

    add capture abstract method to namespace

--- src/Pagador/Transaction/Resource/Facade/FacadeInterface.php
+++ src/Pagador/Transaction/Resource/Facade/FacadeInterface.php
@@ -5,6 +5,7 @@ namespace Webjump\Braspag\Pagador\Transaction\Resource\Facade;
 
 use Webjump\Braspag\Pagador\Transaction\Api\Billet\Send\RequestInterface as BilletRequest;
 use Webjump\Braspag\Pagador\Transaction\Api\CreditCard\Send\RequestInterface as CreditCardRequest;
+use Webjump\Braspag\Pagador\Transaction\Api\CreditCard\Capture\RequestInterface as CaptureCreditCardRequest;
 use Webjump\Braspag\Pagador\Transaction\Api\Debit\Send\RequestInterface as DebitRequest;
 
 interface FacadeInterface
@@ -12,6 +13,8 @@ interface FacadeInterface
     public function sendBillet(BilletRequest $request);
 
     public function sendCreditCard(CreditCardRequest $request);
+
+    public function captureCreditCard(CaptureCreditCardRequest $request);
 
     public function sendDebit(DebitRequest $request);
 }
